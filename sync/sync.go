0f242f2c1e31e8bc4bd2c6ef89243592e1811b93
leonnicolas <60091705+leonnicolas@users.noreply.github.com>
2024-03-01 18:04:55 +0100

    Support syncing users from google groups (#2)
    * Support syncing users from google groups
    
    * Regenerate with fixed swagger spec
    
    * Skip creating nuki users for suspended google users
    
    * Cross compile

--- sync/sync.go
+++ sync/sync.go
@@ -8,6 +8,7 @@ import (
 	"github.com/leonnicolas/gooki/google"
 	"github.com/leonnicolas/gooki/nuki"
 	"github.com/leonnicolas/gooki/nuki/models"
+	admin "google.golang.org/api/admin/directory/v1"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -61,7 +62,7 @@ func DoSync(ctx context.Context, cfg *config.Config) error {
 	c := New(cfg, nukiClient, googleClient)
 
 	log.Info("syncing")
-	err = c.SyncUsers(ctx, cfg.UserMatch)
+	err = c.SyncUsers(ctx, cfg.Query)
 	if err != nil {
 		return err
 	}
@@ -69,50 +70,59 @@ func DoSync(ctx context.Context, cfg *config.Config) error {
 	return nil
 }
 
-func (s *syncGSuite) removeDeletedUsers(ctx context.Context) error {
+func (s *syncGSuite) deletedUsers(ctx context.Context) ([]*admin.User, error) {
 	log.Debug("get deleted users")
 	deletedUsers, err := s.google.GetDeletedUsers()
 	if err != nil {
 		log.Warn("Error Getting Deleted Users", "error", err.Error())
-		return err
+		return nil, err
 	}
-
-	for _, u := range deletedUsers {
-		log := log.WithField("email", u.PrimaryEmail)
-		log.Info("deleting google user")
-
-		uu, err := s.nukiclient.FindUserByEmail(ctx, u.PrimaryEmail)
-		if err != nuki.ErrUserNotFound && err != nil {
-			log.Warn("Error deleting google user")
-			return err
-		}
-
-		if err == nuki.ErrUserNotFound {
-			log.Debug("User already deleted")
-			continue
-		}
-		err = s.nukiclient.DeleteUser(ctx, uu)
-		if err != nil {
-			log.Warn("Error deleting user")
-			return err
-		}
-	}
-	return nil
+	return deletedUsers, nil
 }
 
-func (s *syncGSuite) SyncNewUsers(ctx context.Context, query string) error {
+func (s *syncGSuite) googleUsers(ctx context.Context, query string) ([]*admin.User, error) {
+	var googleUsers []*admin.User
 	log.Debug("get active google users")
-	googleUsers, err := s.google.GetUsers(query)
-	if err != nil {
-		return err
+	if s.cfg.SyncMethod == config.DefaultSyncMethod {
+		log.WithField("query", query).Info("get google groups")
+		googleGroups, err := s.google.GetGroups(query)
+		if err != nil {
+			return nil, err
+		}
+		filteredGoogleGroups := []*admin.Group{}
+		for _, g := range googleGroups {
+			if s.ignoreGroup(g.Email) {
+				log.WithField("group", g.Email).Debug("ignoring group")
+				continue
+			}
+			filteredGoogleGroups = append(filteredGoogleGroups, g)
+		}
+		googleUsers, _, err = s.getGoogleGroupsAndUsers(filteredGoogleGroups)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		var err error
+		googleUsers, err = s.google.GetUsers(query)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	log.Debug("found google users count=", len(googleUsers))
+	return googleUsers, nil
+}
 
+func (s *syncGSuite) createNukiUsers(ctx context.Context, googleUsers []*admin.User) error {
 	for _, u := range googleUsers {
 		ll := log.WithField("email", u.PrimaryEmail)
 		if s.ignoreUser(u.PrimaryEmail) {
 			ll.Debug("ignoring user")
+			continue
+		}
+
+		if u.Suspended {
+			//	deletedUsers = append(deletedUsers, u)
 			continue
 		}
 
@@ -152,17 +162,116 @@ func (s *syncGSuite) SyncNewUsers(ctx context.Context, query string) error {
 }
 
 func (s *syncGSuite) SyncUsers(ctx context.Context, query string) error {
-	if err := s.removeDeletedUsers(ctx); err != nil {
-		log.WithError(err).Error("failed to remove deleted users")
+	deletedUsers, err := s.deletedUsers(ctx)
+	if err != nil {
+		return err
+	}
+	googleUsers, err := s.googleUsers(ctx, query)
+	if err != nil {
 		return err
 	}
 
-	if err := s.SyncNewUsers(ctx, query); err != nil {
+	if err := s.createNukiUsers(ctx, googleUsers); err != nil {
 		log.WithError(err).Error("failed to sync new users")
 		return err
 	}
 
+	for _, u := range googleUsers {
+		if u.Suspended {
+			deletedUsers = append(deletedUsers, u)
+		}
+	}
+
+	if err := s.deleteUsers(ctx, deletedUsers); err != nil {
+		return fmt.Errorf("failed to delete nuki users: %w", err)
+	}
+
 	return nil
+}
+
+func (s *syncGSuite) deleteUsers(ctx context.Context, googleUsers []*admin.User) error {
+	for _, u := range googleUsers {
+		log := log.WithField("email", u.PrimaryEmail)
+		log.Info("deleting google user")
+
+		uu, err := s.nukiclient.FindUserByEmail(ctx, u.PrimaryEmail)
+		if err != nuki.ErrUserNotFound && err != nil {
+			log.Warn("Error deleting google user")
+			return err
+		}
+
+		if err == nuki.ErrUserNotFound {
+			log.Debug("User already deleted")
+			continue
+		}
+		err = s.nukiclient.DeleteUser(ctx, uu)
+		if err != nil {
+			log.Warn("Error deleting user")
+			return err
+		}
+	}
+	return nil
+}
+
+// getGoogleGroupsAndUsers return a list of google users members of googleGroups
+// and a map of google groups and its users' list
+func (s *syncGSuite) getGoogleGroupsAndUsers(googleGroups []*admin.Group) ([]*admin.User, map[string][]*admin.User, error) {
+	gUsers := make([]*admin.User, 0)
+	gGroupsUsers := make(map[string][]*admin.User)
+
+	gUniqUsers := make(map[string]*admin.User)
+
+	for _, g := range googleGroups {
+
+		log := log.WithFields(log.Fields{"group": g.Name})
+
+		if s.ignoreGroup(g.Email) {
+			log.Debug("ignoring group")
+			continue
+		}
+
+		log.Debug("get group members from google")
+		groupMembers, err := s.google.GetGroupMembers(g)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		log.Debug("get users")
+		membersUsers := make([]*admin.User, 0)
+
+		for _, m := range groupMembers {
+
+			if s.ignoreUser(m.Email) {
+				log.WithField("id", m.Email).Debug("ignoring user")
+				continue
+			}
+
+			log.WithField("id", m.Email).Debug("get user")
+			q := fmt.Sprintf("email:%s", m.Email)
+			u, err := s.google.GetUsers(q) // TODO: implement GetUser(m.Email)
+			if err != nil {
+				return nil, nil, err
+			}
+			if len(u) == 0 {
+				log.WithField("id", m.Email).Warn("missing user")
+				continue
+			}
+
+			membersUsers = append(membersUsers, u[0])
+
+			_, ok := gUniqUsers[m.Email]
+			if !ok {
+				gUniqUsers[m.Email] = u[0]
+			}
+		}
+		gGroupsUsers[g.Name] = membersUsers
+	}
+
+	for _, user := range gUniqUsers {
+		gUsers = append(gUsers, user)
+	}
+
+	return gUsers, gGroupsUsers, nil
 }
 
 func (s *syncGSuite) createAuthUser(ctx context.Context, u *nuki.User) error {
@@ -187,6 +296,16 @@ func (s *syncGSuite) createAuthUser(ctx context.Context, u *nuki.User) error {
 
 func ptr[E any](e E) *E {
 	return &e
+}
+
+func (s *syncGSuite) ignoreGroup(name string) bool {
+	for _, g := range s.cfg.IgnoreGroups {
+		if g == name {
+			return true
+		}
+	}
+
+	return false
 }
 
 func (s *syncGSuite) ignoreUser(name string) bool {
--- tools.go
+++ tools.go
@@ -6,6 +6,7 @@ package main
 import (
 	_ "github.com/campoy/embedmd"
 	_ "github.com/deepmap/oapi-codegen/cmd/oapi-codegen"
+	_ "github.com/evanphx/json-patch/cmd/json-patch"
 	_ "github.com/go-swagger/go-swagger/cmd/swagger"
 	_ "github.com/vektra/mockery/v2"
 	_ "github.com/zitadel/oidc/v3/example/server"
