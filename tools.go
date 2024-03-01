b1564e01733aebdb57ff5a87fe3a59cbdc4f211d
leonnicolas <60091705+leonnicolas@users.noreply.github.com>
2024-03-01 16:26:57 +0100

    Add mocks and tests (#4)
    Also small refactor of sync package to be able to test easier.

--- tools.go
+++ tools.go
@@ -7,5 +7,6 @@ import (
 	_ "github.com/campoy/embedmd"
 	_ "github.com/deepmap/oapi-codegen/cmd/oapi-codegen"
 	_ "github.com/go-swagger/go-swagger/cmd/swagger"
+	_ "github.com/vektra/mockery/v2"
 	_ "github.com/zitadel/oidc/v3/example/server"
 )
